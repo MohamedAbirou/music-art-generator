c8fa8c1ce0cd1d28b209c58691e0f6118a055e3f
Mohamed Abirou <abiroumohamed58@gmail.com>
2024-05-07 15:48:39 +0100

    glhf

--- backend/src/config/corsOptions.ts
+++ backend/src/config/corsOptions.ts
@@ -1,15 +1,23 @@
 import "dotenv/config";
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://music-art-generator.web.app",
+  "http://localhost:3000",
+];
+
 // Enable CORS (Cross-Origin Resource Sharing)
 export const corsOptions = {
   origin: function (
-    origin: string | undefined = process.env.FRONTEND_URL,
+    origin: string | undefined,
     callback: (err: Error | null, allow?: boolean) => void
   ) {
-    if (origin) {
+    if (allowedOrigins.includes(origin!)) {
+      console.log("this is your origin", origin);
       callback(null, true);
     } else {
-      callback(new Error("Not allowed by CORS! LOL!"));
+      console.log("this is your origin Error", origin);
+      callback(new Error("Not allowed by CORS ha zebi"));
     }
   }, // Allow requests from this origin
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Specify the HTTP methods you want to allow
